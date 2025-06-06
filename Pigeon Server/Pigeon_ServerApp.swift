3f44bda29172ffc7e1cd45a665321aad1fb9bf47
matteo ianni <iannimatte@gmail.com>
2025-06-06 09:11:05 -0400

    Initial Commit

---
+++ Pigeon Server/Pigeon_ServerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pigeon_ServerApp.swift
+//  Pigeon Server
+//
+//  Created by matteo ianni on 06/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Pigeon_ServerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
